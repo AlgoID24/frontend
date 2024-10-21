8bf1457271fc406a1fb9d320c7a0fe17b31c1d99
Ezekiel Victor <codeepoch@gmail.com>
2024-10-21 20:04:30 +0100

    setup unplugin and add icons to app navigation

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,18 @@
+import Icons from "unplugin-icons/webpack";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack(config) {
+    config.plugins.push(
+      Icons({
+        jsx: "react",
+        compiler: "jsx",
+        autoInstall: true,
+      }),
+    );
+    return config;
+  },
+};
 //
 
 export default nextConfig;
